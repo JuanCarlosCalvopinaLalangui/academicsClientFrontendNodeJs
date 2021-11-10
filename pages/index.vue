87927e9c532fc560651b07bfb41192eefd330974
Juan Carlos <2172742@my.ipleira.pt>
2021-11-10 01:40:26 +0000

    Setup Front-end Work Sheet 3

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <!-- easy components usage, already shipped with bootstrap css-->
-  <b-container>
-    <!-- try to remove :fields=”fields” to see the magic -->
-    <b-table striped over :items="students" :fields="fields" />
-  </b-container>
+  <div>
+    <!-- easy components usage, already shipped with bootstrap css-->
+    <b-container>
+      <!-- try to remove :fields=”fields” to see the magic -->
+      <b-table striped over :items="students" :fields="fields" />
+    </b-container>
+    <nuxt-link to="/createStudent">Create a New Student</nuxt-link>
+    <nuxt-link to="/createCourse">Create a New Course</nuxt-link>
+  </div>
 </template>
 
 <script>
